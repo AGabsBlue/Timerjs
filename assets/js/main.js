efc96480a7b82135c2341b2ddb43fc59053fd6c3
AGabs <anagabriellaa86@gmail.com>
2022-09-21 16:41:26 -0300

    Finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,39 @@
-alert(1)
+function criaHoraDosSegundos(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleDateString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio(){
+    timer = setInterval(function() {
+        segundos++; //Para somar segundos
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000); //1000ms = 1s
+} //Aqui vai formatar vai formatar a hora/minutos/segundos
+
+
+iniciar.addEventListener('click', function(event){
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+
+pausar.addEventListener('click', function(event){
+    clearInterval(timer);
+    relogio.classList.add('pausado');
+});
+
+zerar.addEventListener('click', function(event){
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
